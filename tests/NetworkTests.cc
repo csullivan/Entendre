f5cb2292402239d7602df1195ac31de04f761a11
Chris Sullivan <sullivan@nscl.msu.edu>
2016-09-13 00:13:34 -0400

    Added performance test for a large network. 10 input nodes fully connected to 20 hidden nodes, which are fully connected to 10 output nodes. After 100 trials average times were:
    Average time to construct network: 0.182294 ms
    
    Average time to evaluate network: 0.00532954 ms
    
    In words, it's blazing fast!

--- tests/NetworkTests.cc
+++ tests/NetworkTests.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "Genome.hh"
 #include "NeuralNet.hh"
+#include "Timer.hh"
 
 TEST(NeuralNet,EvaluateNetwork){
     auto sigmoid = [](double val) {return 1/(1 + std::exp(-val));};
@@ -43,5 +44,82 @@ TEST(NeuralNet,EvaluateRecurrentNetwork){
         result2[0],
         sigmoid(1.5+sigmoid(0.5 + result[0]))
         );
+
+}
+
+TEST(NeuralNet,EvaluateLargeNetwork){
+    auto sigmoid = [](double val) {return 1/(1 + std::exp(-val));};
+
+    auto genome = Genome().
+        AddNode(NodeType::Bias);
+
+    auto nInputs = 10u;
+    auto nHidden = 20u;
+    auto nOutputs = 10u;
+
+    auto nTotal = nInputs + nHidden + nOutputs + 1;
+
+    for (auto i=0u;i<nInputs; i++) {
+        genome.AddNode(NodeType::Input);
+    }
+    for (auto i=0u;i<nHidden; i++) {
+        genome.AddNode(NodeType::Hidden);
+    }
+    for (auto i=0u;i<nOutputs; i++) {
+        genome.AddNode(NodeType::Output);
+    }
+
+
+    // Connect every input node to every hidden node
+    for (auto i=0u;i<nInputs+1; i++) {
+        for (auto j=nInputs+1;j<nHidden+nInputs+1; j++) {
+            genome.AddGene(i,j,ConnectionType::Normal,true,1.);
+        }
+    }
+
+    // Connect every hidden node to every output node
+    for (auto j=nInputs+1;j<nHidden+nInputs+1; j++) {
+        for (auto k=nHidden+nInputs+1; k<nTotal; k++) {
+            genome.AddGene(j,k,ConnectionType::Normal,true,1.);
+        }
+    }
+
+    auto nTrials = 100u;
+    double tperformance = 0.0;
+
+
+
+    // Time the network construction
+    for (auto i=0u; i < nTrials; i++)
+    {
+        Timer tbuild([&tperformance](int elapsed) {
+                tperformance+=elapsed;
+        });
+        auto net = NeuralNet(genome);
+    }
+    std::cout << std:: endl
+              << "Average time to construct network: "
+              << tperformance/nTrials/1.0e6 << " ms\n"
+              << std::endl;
+    tperformance = 0.0;
+
+
+    // Time the network evaluation
+    for (auto i=0u; i < nTrials; i++)
+    {
+        auto net = NeuralNet(genome);
+        net.register_sigmoid(sigmoid);
+
+        Timer teval([&tperformance](int elapsed) {
+                tperformance+=elapsed;
+        });
+
+        auto result = net.evaluate({0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5});
+    }
+    std::cout << std:: endl
+              << "Average time to evaluate network: "
+              << tperformance/nTrials/1.0e6 << " ms\n"
+              << std::endl;
+
 
 }
