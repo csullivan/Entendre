9073f22dc4a08395119fd53e3722fb1fd063fedc
Chris Sullivan <sullivan@nscl.msu.edu>
2017-04-29 20:18:28 -0400

    Updated python performance script and added performance results from MSU HPCC GPU cluster.

--- performance/performance.py
+++ performance/performance.py
@@ -1,10 +1,13 @@
-#!/opt/software/anaconda3/bin/python
+#!/usr/bin/python
 
 import subprocess
 if __name__=="__main__":
     for num_networks in [pow(2,n) for n in range(10,21)]:
-        completed = subprocess.run(
-            ['../bin/performance_tests', '-n',str(num_networks)],
-            stdout=subprocess.PIPE,
-        )
-        print(completed.stdout.decode('utf-8'))
+        try:
+            completed = subprocess.run(
+                ['../bin/performance_tests', '-n',str(num_networks)],
+                stdout=subprocess.PIPE,
+            )
+            print(completed.stdout.decode('utf-8'))
+        except:
+            subprocess.call(['../bin/performance_tests', '-n',str(num_networks)])
