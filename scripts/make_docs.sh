40ce8547cbe46b07cd27c5b15e64a74466234df1
Eldritch Cheese <eldritch.cheese@gmail.com>
2017-05-17 08:16:00 -0400

    Set up Travis CI to autogenerate the documentation.

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
 cd ..
 
@@ -12,7 +14,7 @@ doxygen scripts/doxy.config
 
 # Run clang static analysis
 scons -c
-scan-build -o docs/temp scons
+scan-build-4.0 --use-c++=clang++-4.0 -o docs/temp scons
 # Move output directory to correct location
 mv docs/temp/* docs/clang-static-analysis
 rmdir docs/temp
@@ -21,15 +23,16 @@ rmdir docs/temp
 
 # Compile with gcov
 scons -c
-scons CODE_COVERAGE=1
+CXX=g++-6 scons CODE_COVERAGE=1 VERBOSE=1
+lcov --version
 # Collect stats
-lcov --base-directory . --directory . --zerocounters -q
+lcov --gcov-tool gcov-6 --base-directory . --directory . --zerocounters -q
 bin/entendre_tests
-lcov --base-directory . --directory . -c -o docs/temp.info
+lcov --gcov-tool gcov-6 --base-directory . --directory . -c -o docs/temp.info
 # Clear library dependencies
-lcov --remove docs/temp.info "/usr*" -o docs/temp.info
-lcov --remove docs/temp.info "build/.dependencies*" -o docs/temp.info
+lcov --remove docs/temp.info "/usr/*" -o docs/temp.info
+lcov --remove docs/temp.info "*/build/.dependencies/*" -o docs/temp.info
 # Generate HTML
 rm -rf docs/code-coverage
 genhtml -o docs/code-coverage -t "Entendre code coverage" docs/temp.info
-mv docs/temp.info docs/code-coverage
+mv docs/temp.info docs/code-coverage/data.info
